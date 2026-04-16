685a930e914e12b39e686c873368a00f1dac8c57
jepu <jepu20@student.bth.se>
2026-04-16 12:10:28 +0200

    jobb 16/4
    börjar med mocking nästa

--- test_estimate_pi.py
+++ test_estimate_pi.py
@@ -1,15 +1,24 @@
 import unittest
 from estimate_pi import estimate_pi, PiFileWriter
+import time
 
 class TestEstimatePi(unittest.TestCase):
     def test_estimate_pi(self):
         pi_expected = 3.141592653589793
         pi_actual = estimate_pi(1000000)
         self.assertAlmostEqual(pi_expected, pi_actual, delta=0.01) #repeatable
-        
-        self.assertGreater(pi_actual, 3.14) # our
-        self.assertLess(pi_actual, 3.15) # our
 
+    def test_boundary(self): #Test1
+        pi_actual = estimate_pi(1000000)
+        self.assertGreater(pi_actual, 3.13)
+        self.assertLess(pi_actual, 3.15) 
+
+    def test_fast(self): #Test2
+        start_time = time.time()
+        self.test_estimate_pi()
+        end_time = time.time()
+        # self.assertTrue((end_time - start_time) > 100)
+        print("time: ", end_time - start_time)
 
 if __name__ == '__main__':
     unittest.main()
